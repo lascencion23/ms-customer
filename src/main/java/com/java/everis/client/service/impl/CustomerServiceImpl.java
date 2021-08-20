fe71db9edd415984e25b566206ee4cefa446a1cf
DAlembert721 <dalembert.dmu@gmail.com>
2021-08-19 21:22:00 -0500

    feat: update config

--- src/main/java/com/java/everis/client/service/impl/CustomerServiceImpl.java
+++ src/main/java/com/java/everis/client/service/impl/CustomerServiceImpl.java
@@ -19,7 +19,7 @@ public class CustomerServiceImpl implements CustomerService {
 	private final WebClient webClient;
 	private final ReactiveCircuitBreaker reactiveCircuitBreaker;
 	
-	String uri = "http://localhost:8090/api/ms-profile/typecustomer/find/{id}";
+	String uri = "http://gateway:8090/api/ms-profile/typecustomer/find/{id}";
 	
 	public CustomerServiceImpl(ReactiveResilience4JCircuitBreakerFactory circuitBreakerFactory) {
 		this.webClient = WebClient.builder().baseUrl(this.uri).build();
